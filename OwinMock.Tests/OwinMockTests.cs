2cf340b91d3fe0bdb07dedbc0376dda108f5c1a3
Max Brewer <max.brewer@acumenci.com>
2017-03-14 19:35:05 +0000

    Added License and Readme to Solution

--- OwinMock.Tests/OwinMockTests.cs
+++ OwinMock.Tests/OwinMockTests.cs
@@ -139,20 +139,6 @@ namespace OwinMock.Tests
             Assert.Equal(1, callCount);
         }
 
-        /// Needs to work with all methods
-        /// Work with multiple ports from one owin mock? Then it cleans them all up together.
-        /// Which methods have request bodies? - Maybe have various request builders for different request types.
-        /// Need to be able to interrogate the request for matching.
-        /// * On query parameters
-        /// * On headers
-        /// * On request body
-        /// Need to record the requests. - Then you can have a look later/do some assertions.
-        /// Maybe get rid of dependencies on Microsoft.Owin and Owin.Hosting (Owin.Hosting can be a dependency, but not of the core module).
-        /// Https?
-        /// Various kinds of auth?
-        /// Various helper methods for different request types
-        /// Fluent API
-
         public void Dispose()
         {
             _server?.Dispose();
